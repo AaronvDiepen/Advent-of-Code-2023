049feab2adb0268cceebf15f8b029766a16b908b
AaronvDiepen <aaronvdiepen@gmail.com>
2023-12-09 18:56:17 +0100

    Switch binomial to use iterator approach

--- day9/part1/src/main.rs
+++ day9/part1/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 use rayon::prelude::*;
-use num_integer::binomial;
+use num_integer::IterBinomial;
 
 // Function that processes each line
 fn process_line(line: String) -> i64 {
@@ -11,17 +11,24 @@ fn process_line(line: String) -> i64 {
         .collect();
 
     // Use the row of pascals triangle that is equal to the length of our input to calculate the new entry
-    first_row.iter().enumerate()
-        .map(|(i, entry)| {
-            if i % 2 == 0 {
-                // For even indexed entries we need to add the entry times the value from pascals triangle
-                binomial(first_row.len(), i) as i64 * entry
+    first_row.iter()
+        // Multiply the entry with the values in the corresponding row of pascals triangle
+        .zip(IterBinomial::new(first_row.len()))
+        .map(|(entry, binomial)| {
+            binomial as i64 * entry
+        })
+        // Combine the values into a new entry
+        // We need to allow wrapping since partial sums can get very large
+        .enumerate()
+        .fold(0, |sum, (index, value)| {
+            if index & 1 == 0 {
+                // For even indexed values we need to add the value to the total
+                sum.wrapping_add(value)
             } else {
-                // For odd indexed entries we need to subtract the entry times the value from pascals triangle
-                -1 * binomial(first_row.len(), i) as i64 * entry
+                // For odd indexed values we need to subtract the value from the total
+                sum.wrapping_sub(value)
             }
         })
-        .fold(0, |acc, num| acc.wrapping_add(num))
 }
 
 fn main() {
--- day9/part2/src/main.rs
+++ day9/part2/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 use rayon::prelude::*;
-use num_integer::binomial;
+use num_integer::IterBinomial;
 
 // Function that processes each line
 fn process_line(line: String) -> i64 {
@@ -11,18 +11,24 @@ fn process_line(line: String) -> i64 {
         .collect();
 
     // Use the row of pascals triangle that is equal to the length of our input to calculate the new entry
-    first_row.iter().enumerate()
-        .map(|(i, entry)| {
-            if i % 2 == 0 {
-                // For even indexed entries we need to add the entry times the next value from pascals triangle
-                binomial(first_row.len(), i + 1) as i64 * entry
+    first_row.iter()
+        // Multiply the entry with the values in the corresponding row of pascals triangle after skipping 1 entry
+        .zip(IterBinomial::new(first_row.len()).skip(1))
+        .map(|(entry, binomial)| {
+            binomial as i64 * entry
+        })
+        // Combine the values into a new entry
+        // We need to allow wrapping since partial sums can get very large
+        .enumerate()
+        .fold(0, |sum, (index, value)| {
+            if index & 1 == 0 {
+                // For even indexed values we need to add the value to the total
+                sum.wrapping_add(value)
             } else {
-                // For odd indexed entries we need to subtract the entry times the next value from pascals triangle
-                -1 * binomial(first_row.len(), i + 1) as i64 * entry
+                // For odd indexed values we need to subtract the value from the total
+                sum.wrapping_sub(value)
             }
         })
-        // We need to allow wrapping since partial sums can get very large
-        .fold(0, |acc, num| acc.wrapping_add(num))
 }
 
 fn main() {
